0c99e4a47473f4acb9929e07b8a17e1e1ee4ad8b
lfs736012 <lfs736012@163.com>
2016-04-25 10:37:32 +0800

    this first commit

---
+++ LFCircleMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LFCircleMenu
+//
+//  Created by lifushuai on 16/4/25.
+//  Copyright © 2016年 lifushuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LFCircleMenu/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LFCircleMenu
+//
+//  Created by lifushuai on 16/4/25.
+//  Copyright © 2016年 lifushuai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
